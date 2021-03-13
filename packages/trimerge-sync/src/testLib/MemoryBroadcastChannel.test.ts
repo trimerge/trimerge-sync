84fc44118bf1494decb1c5074b3cd7a69bea749c
Marcello Bastéa-Forte <marcello@descript.com>
2021-03-13 10:31:15 -0800

    add leadership support to MemoryBroadcastChannel

---
+++ packages/trimerge-sync/src/testLib/MemoryBroadcastChannel.test.ts
@@ -0,0 +1,206 @@
+import { MemoryBroadcastChannel, resetAll } from './MemoryBroadcastChannel';
+import { timeout } from './MergeUtils';
+
+afterEach(() => {
+  resetAll();
+});
+
+describe('MemoryBroadcastChannel', () => {
+  it('broadcasts with two', async () => {
+    const on1 = jest.fn();
+    const bc1 = new MemoryBroadcastChannel<string>('test', on1);
+    const on2 = jest.fn();
+    const bc2 = new MemoryBroadcastChannel<string>('test', on2);
+    await bc1.postMessage('hi');
+    expect(on1.mock.calls).toMatchInlineSnapshot(`Array []`);
+    expect(on2.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "hi",
+        ],
+      ]
+    `);
+    await bc2.postMessage('there');
+    expect(on1.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "there",
+        ],
+      ]
+    `);
+    expect(on2.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "hi",
+        ],
+      ]
+    `);
+  });
+  it('closing closes', async () => {
+    const on1 = jest.fn();
+    const bc1 = new MemoryBroadcastChannel<string>('test', on1);
+    const on2 = jest.fn();
+    const bc2 = new MemoryBroadcastChannel<string>('test', on2);
+    bc1.close();
+    await bc2.postMessage('yo');
+    expect(on1.mock.calls).toMatchInlineSnapshot(`Array []`);
+    expect(on2.mock.calls).toMatchInlineSnapshot(`Array []`);
+  });
+
+  it('throw on post after close', async () => {
+    const on = jest.fn();
+    const bc = new MemoryBroadcastChannel<string>('test', on);
+    bc.close();
+    await expect(
+      bc.postMessage('yo'),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(`"already closed"`);
+  });
+
+  it('ok to double close', async () => {
+    const on = jest.fn();
+    const bc = new MemoryBroadcastChannel<string>('test', on);
+    bc.close();
+    bc.close();
+  });
+
+  it('await leadership', async () => {
+    const on = jest.fn();
+    const bc = new MemoryBroadcastChannel<string>('test', on);
+    await bc.awaitLeadership();
+    bc.close();
+  });
+
+  it('await leadership with two', async () => {
+    const events: string[] = [];
+    const bc1 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const bc2 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const p1 = bc1
+      .awaitLeadership()
+      .then(() => events.push('bc1 awaitLeadership'));
+    const p2 = bc2
+      .awaitLeadership()
+      .then(() => events.push('bc2 awaitLeadership'));
+    await p1;
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+      ]
+    `);
+    bc1.close();
+    await p2;
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+        "bc2 awaitLeadership",
+      ]
+    `);
+    bc2.close();
+    await timeout();
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+        "bc2 awaitLeadership",
+      ]
+    `);
+  });
+
+  it('await leadership after close', async () => {
+    const bc1 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    await bc1.awaitLeadership();
+    bc1.close();
+
+    await timeout();
+
+    const bc2 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    await bc2.awaitLeadership();
+    bc2.close();
+  });
+
+  it('await leadership with three', async () => {
+    const events: string[] = [];
+    const bc1 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const bc2 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const bc3 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const p1 = bc1
+      .awaitLeadership()
+      .then(() => events.push('bc1 awaitLeadership'));
+    const p2 = bc2
+      .awaitLeadership()
+      .then(() => events.push('bc2 awaitLeadership'));
+    const p3 = bc3
+      .awaitLeadership()
+      .then(() => events.push('bc3 awaitLeadership'));
+    await p1;
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+      ]
+    `);
+    bc1.close();
+    await p2;
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+        "bc2 awaitLeadership",
+      ]
+    `);
+    bc3.close();
+    await expect(p3).rejects.toThrowErrorMatchingInlineSnapshot(`"closed"`);
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+        "bc2 awaitLeadership",
+      ]
+    `);
+    bc2.close();
+    await timeout();
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+        "bc2 awaitLeadership",
+      ]
+    `);
+  });
+  it('await leadership with three then one more', async () => {
+    const events: string[] = [];
+    const bc1 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const bc2 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const bc3 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const p1 = bc1
+      .awaitLeadership()
+      .then(() => events.push('bc1 awaitLeadership'));
+    const p2 = bc2
+      .awaitLeadership()
+      .then(() => events.push('bc2 awaitLeadership'));
+    const p3 = bc3
+      .awaitLeadership()
+      .then(() => events.push('bc3 awaitLeadership'));
+    await p1;
+    bc1.close();
+    await p2;
+    bc2.close();
+    await p3;
+    bc3.close();
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+        "bc2 awaitLeadership",
+        "bc3 awaitLeadership",
+      ]
+    `);
+    const bc4 = new MemoryBroadcastChannel<string>('test', () => undefined);
+    const p4 = bc4
+      .awaitLeadership()
+      .then(() => events.push('bc4 awaitLeadership'));
+    await p4;
+
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        "bc1 awaitLeadership",
+        "bc2 awaitLeadership",
+        "bc3 awaitLeadership",
+        "bc4 awaitLeadership",
+      ]
+    `);
+  });
+});
--- packages/trimerge-sync/src/testLib/MemoryBroadcastChannel.ts
+++ packages/trimerge-sync/src/testLib/MemoryBroadcastChannel.ts
@@ -1,25 +1,98 @@
 const channels = new Map<string, MemoryBroadcastChannel<any>[]>();
+const leaders = new Map<
+  string,
+  {
+    current: MemoryBroadcastChannel<any>;
+    awaiting: MemoryBroadcastChannel<any>[];
+  }
+>();
+
+export function resetAll() {
+  channels.clear();
+  leaders.clear();
+}
+
+function removeItem<T>(array: T[], item: T): boolean {
+  const index = array.indexOf(item);
+  if (index < 0) {
+    return false;
+  }
+  array.splice(index, 1);
+  return true;
+}
 
 export class MemoryBroadcastChannel<T> {
+  private closed = false;
   private readonly array: MemoryBroadcastChannel<T>[];
-  constructor(channel: string, private readonly onEvent: (value: T) => void) {
+  private resolveLeader?: () => void;
+  private rejectLeader?: (error: Error) => void;
+
+  constructor(
+    private readonly channel: string,
+    private readonly onEvent: (value: T) => void,
+  ) {
     this.array = channels.get(channel) ?? [];
     if (this.array.length === 0) {
       channels.set(channel, this.array);
     }
     this.array.push(this);
   }
+
+  awaitLeadership(): Promise<void> {
+    const leader = leaders.get(this.channel);
+    if (leader) {
+      return new Promise((resolve, reject) => {
+        this.resolveLeader = resolve;
+        this.rejectLeader = reject;
+        leader.awaiting.push(this);
+      });
+    }
+    leaders.set(this.channel, {
+      current: this,
+      awaiting: [],
+    });
+    return Promise.resolve();
+  }
+
   async postMessage(value: T): Promise<void> {
+    if (this.closed) {
+      throw new Error('already closed');
+    }
     for await (const channel of this.array) {
       if (channel !== this) {
         channel.onEvent(value);
       }
     }
   }
-  close() {
-    const index = this.array.indexOf(this);
-    if (index >= 0) {
-      this.array.splice(index, 1);
+  close(): void {
+    if (this.closed) {
+      return;
+    }
+    this.closed = true;
+    removeItem(this.array, this);
+    if (this.rejectLeader) {
+      this.rejectLeader(new Error('closed'));
+      this.rejectLeader = undefined;
+    }
+    this.resolveLeader = undefined;
+    const leader = leaders.get(this.channel);
+    if (!leader) {
+      return;
+    }
+    removeItem(leader.awaiting, this);
+    if (leader.current === this) {
+      const nextLeader = leader.awaiting.shift();
+      if (nextLeader) {
+        if (nextLeader.resolveLeader) {
+          leader.current = nextLeader;
+          nextLeader.resolveLeader();
+          nextLeader.resolveLeader = undefined;
+        }
+        nextLeader.rejectLeader = undefined;
+      } else {
+        // No more leaders
+        leaders.delete(this.channel);
+      }
     }
   }
 }
