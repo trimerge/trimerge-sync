7dd064c19f71530837b653c44dcaecd7c7e4caba
Marcello Bastéa-Forte <marcello@descript.com>
2021-06-15 12:17:45 -0700

    basic-server: more user-configurable logging and URL parsing

--- packages/trimerge-sync-basic-server/src/types.ts
+++ packages/trimerge-sync-basic-server/src/types.ts
@@ -3,4 +3,11 @@ export type AuthenticateFn = (
   docId: string,
   auth: unknown,
 ) => Promise<Authenticated>;
-export type LogFn = (message: string, params?: Record<string, any>) => void;
+export type LogParams = Record<string, number | string | undefined | null>;
+export type LogFn = (message: string, params?: LogParams) => void;
+
+export interface Logger {
+  debug: LogFn;
+  info: LogFn;
+  warn: LogFn;
+}
