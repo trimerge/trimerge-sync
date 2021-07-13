d0475330fb8e9f96c97ce57d9fc83f8edd07b217
Marcello Bastéa-Forte <marcello@descript.com>
2021-07-13 13:19:53 -0700

    externalize sqlite

--- packages/trimerge-sync-basic-server/rollup.config.js
+++ packages/trimerge-sync-basic-server/rollup.config.js
@@ -2,7 +2,7 @@ import pkg from './package.json';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import { dependencies } from './package.json';
+import { dependencies, peerDependencies } from './package.json';
 
 export default {
   input: 'src/index.ts',
@@ -11,7 +11,12 @@ export default {
     resolve({ preferBuiltins: true }),
     typescript({ exclude: '**/*.test.ts' }),
   ],
-  external: Object.keys(dependencies),
+  external: [
+    ...new Set([
+      ...Object.keys(dependencies),
+      ...Object.keys(peerDependencies),
+    ]),
+  ],
   output: [
     {
       file: pkg.main,
