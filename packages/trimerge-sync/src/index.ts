45b4ecbd7ad987412184d5a573aaf92666db061a
Marcello Bastéa-Forte <marcello@descript.com>
2022-01-19 22:26:05 -0800

    fix exports

--- packages/trimerge-sync/src/index.ts
+++ packages/trimerge-sync/src/index.ts
@@ -4,5 +4,4 @@ export * from './types';
 export * from './AbstractLocalStore';
 export * from './validateCommits';
 export * from './lib/PromiseQueue';
-export { MergeDocFn } from './auto-merge-heads';
-export { MergeResult } from './auto-merge-heads';
+export * from './auto-merge-heads';
