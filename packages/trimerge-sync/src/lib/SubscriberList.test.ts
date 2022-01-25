741fff721c7ae096605e5102a9c475e2acb50fb3
Marcello Bastéa-Forte <marcello@descript.com>
2022-01-24 17:23:07 -0800

    update tests

--- packages/trimerge-sync/src/lib/SubscriberList.test.ts
+++ packages/trimerge-sync/src/lib/SubscriberList.test.ts
@@ -5,32 +5,38 @@ describe('SubscriberList', () => {
     let value = 0;
     const s = new SubscriberList(() => value);
     const onChange = jest.fn();
-    const unsub = s.subscribe(onChange);
-    expect(onChange.mock.calls).toEqual([[0]]);
-    s.emitChange(); // no change
+    const unsub = s.subscribe(onChange, '0');
+    expect(onChange.mock.calls).toEqual([[0, '0']]);
+    s.emitChange('1'); // no change
     value = 1;
-    s.emitChange(); // change
-    s.emitChange(); // no change
+    s.emitChange('2'); // change
+    s.emitChange('3'); // no change
     unsub();
     value = 2;
-    s.emitChange(); // change, but no listeners
-    expect(onChange.mock.calls).toEqual([[0], [1]]);
+    s.emitChange('4'); // change, but no listeners
+    expect(onChange.mock.calls).toEqual([
+      [0, '0'],
+      [1, '2'],
+    ]);
   });
   it('two listeners subscribes', () => {
     let value = 0;
     const s = new SubscriberList(() => value);
     const onChange1 = jest.fn();
-    const unsub1 = s.subscribe(onChange1);
-    s.emitChange(); // no change
+    const unsub1 = s.subscribe(onChange1, '0');
+    s.emitChange('1'); // no change
     value = 1;
     const onChange2 = jest.fn();
-    const unsub2 = s.subscribe(onChange2);
-    expect(onChange1.mock.calls).toEqual([[0]]);
-    expect(onChange2.mock.calls).toEqual([[1]]);
-    s.emitChange(); // change
+    const unsub2 = s.subscribe(onChange2, '2');
+    expect(onChange1.mock.calls).toEqual([[0, '0']]);
+    expect(onChange2.mock.calls).toEqual([[1, '2']]);
+    s.emitChange('3'); // change
     unsub1();
     unsub2();
-    expect(onChange1.mock.calls).toEqual([[0], [1]]);
-    expect(onChange2.mock.calls).toEqual([[1]]);
+    expect(onChange1.mock.calls).toEqual([
+      [0, '0'],
+      [1, '3'],
+    ]);
+    expect(onChange2.mock.calls).toEqual([[1, '2']]);
   });
 });
