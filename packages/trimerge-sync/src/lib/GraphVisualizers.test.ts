604230c294c0c88d775efb18564a03f4919387ed
Matthew Keller <matthew@descript.com>
2022-10-17 09:36:55 -0400

    update snapshots

--- packages/trimerge-sync/src/lib/GraphVisualizers.test.ts
+++ packages/trimerge-sync/src/lib/GraphVisualizers.test.ts
@@ -112,8 +112,8 @@ describe('GraphVisualizers', () => {
     `);
     expect(dotGraph(store, client1)).toMatchInlineSnapshot(`
       "digraph {
-      \\"X1xFORPw\\" [shape=ellipse, label=\\"X1xFORPw\\", color=black, fillcolor=azure, style=filled]
-      \\"OscPQkG7:F7kQ39Rs\\" [shape=ellipse, label=\\"OscPQkG7:F7kQ39Rs (2 commits)\\", color=black, fillcolor=azure, style=filled]
+      \\"X1xFORPw\\" [shape=ellipse, label=\\"X1xFORPw\\", color=black, fillcolor=azure, style=filled, id=\\"X1xFORPw\\"];
+      \\"OscPQkG7:F7kQ39Rs\\" [shape=ellipse, label=\\"OscPQkG7:F7kQ39Rs (2 commits)\\", color=black, fillcolor=azure, style=filled, id=\\"F7kQ39Rs\\"];
       }"
     `);
   });
@@ -148,8 +148,8 @@ describe('GraphVisualizers', () => {
     expect(getTestDotGraph(commits, (commit) => commit.metadata).graph)
       .toMatchInlineSnapshot(`
       "digraph {
-      \\"1:3\\" [shape=ellipse, label=\\"1:3 (3 commits)\\", color=black, fillcolor=azure, style=filled]
-      \\"4\\" [shape=ellipse, label=\\"4\\", color=black, fillcolor=azure, style=filled]
+      \\"1:3\\" [shape=ellipse, label=\\"1:3 (3 commits)\\", color=black, fillcolor=azure, style=filled, id=\\"3\\"];
+      \\"4\\" [shape=ellipse, label=\\"4\\", color=black, fillcolor=azure, style=filled, id=\\"4\\"];
       \\"1:3\\" -> \\"4\\" [label=\\"fourth\\"]
       }"
     `);
@@ -186,12 +186,12 @@ describe('GraphVisualizers', () => {
     expect(getTestDotGraph(commits, (commit) => commit.metadata).graph)
       .toMatchInlineSnapshot(`
       "digraph {
-      \\"1\\" [shape=ellipse, label=\\"1\\", color=black, fillcolor=azure, style=filled]
-      \\"2\\" [shape=ellipse, label=\\"2\\", color=black, fillcolor=azure, style=filled]
+      \\"1\\" [shape=ellipse, label=\\"1\\", color=black, fillcolor=azure, style=filled, id=\\"1\\"];
+      \\"2\\" [shape=ellipse, label=\\"2\\", color=black, fillcolor=azure, style=filled, id=\\"2\\"];
       \\"1\\" -> \\"2\\" [label=\\"second\\"]
-      \\"3\\" [shape=ellipse, label=\\"3\\", color=black, fillcolor=azure, style=filled]
+      \\"3\\" [shape=ellipse, label=\\"3\\", color=black, fillcolor=azure, style=filled, id=\\"3\\"];
       \\"1\\" -> \\"3\\" [label=\\"third\\"]
-      \\"4\\" [shape=rectangle, label=\\"4\\", color=black, fillcolor=azure, style=filled]
+      \\"4\\" [shape=rectangle, label=\\"4\\", color=black, fillcolor=azure, style=filled, id=\\"4\\"];
       \\"3\\" -> \\"4\\" [label=left]
       \\"2\\" -> \\"4\\" [label=right]
       }"
@@ -259,22 +259,22 @@ describe('GraphVisualizers', () => {
     expect(getTestDotGraph(commits, (commit) => commit.metadata).graph)
       .toMatchInlineSnapshot(`
       "digraph {
-      \\"1\\" [shape=ellipse, label=\\"1\\", color=black, fillcolor=azure, style=filled]
-      \\"2\\" [shape=ellipse, label=\\"2\\", color=black, fillcolor=azure, style=filled]
+      \\"1\\" [shape=ellipse, label=\\"1\\", color=black, fillcolor=azure, style=filled, id=\\"1\\"];
+      \\"2\\" [shape=ellipse, label=\\"2\\", color=black, fillcolor=azure, style=filled, id=\\"2\\"];
       \\"1\\" -> \\"2\\" [label=\\"second\\"]
-      \\"3\\" [shape=ellipse, label=\\"3\\", color=black, fillcolor=azure, style=filled]
+      \\"3\\" [shape=ellipse, label=\\"3\\", color=black, fillcolor=azure, style=filled, id=\\"3\\"];
       \\"2\\" -> \\"3\\" [label=\\"third\\"]
-      \\"4\\" [shape=ellipse, label=\\"4\\", color=black, fillcolor=azure, style=filled]
+      \\"4\\" [shape=ellipse, label=\\"4\\", color=black, fillcolor=azure, style=filled, id=\\"4\\"];
       \\"3\\" -> \\"4\\" [label=\\"fourth\\"]
-      \\"5\\" [shape=ellipse, label=\\"5\\", color=black, fillcolor=azure, style=filled]
+      \\"5\\" [shape=ellipse, label=\\"5\\", color=black, fillcolor=azure, style=filled, id=\\"5\\"];
       \\"4\\" -> \\"5\\" [label=\\"fifth\\"]
-      \\"3prime\\" [shape=ellipse, label=\\"3prime\\", color=black, fillcolor=azure, style=filled]
+      \\"3prime\\" [shape=ellipse, label=\\"3prime\\", color=black, fillcolor=azure, style=filled, id=\\"3prime\\"];
       \\"2\\" -> \\"3prime\\" [label=\\"fifth\\"]
-      \\"4prime\\" [shape=ellipse, label=\\"4prime\\", color=black, fillcolor=azure, style=filled]
+      \\"4prime\\" [shape=ellipse, label=\\"4prime\\", color=black, fillcolor=azure, style=filled, id=\\"4prime\\"];
       \\"3prime\\" -> \\"4prime\\" [label=\\"sixth\\"]
-      \\"5prime\\" [shape=ellipse, label=\\"5prime\\", color=black, fillcolor=azure, style=filled]
+      \\"5prime\\" [shape=ellipse, label=\\"5prime\\", color=black, fillcolor=azure, style=filled, id=\\"5prime\\"];
       \\"4prime\\" -> \\"5prime\\" [label=\\"seventh\\"]
-      \\"merged4s\\" [shape=rectangle, label=\\"merged4s\\", color=black, fillcolor=azure, style=filled]
+      \\"merged4s\\" [shape=rectangle, label=\\"merged4s\\", color=black, fillcolor=azure, style=filled, id=\\"merged4s\\"];
       \\"4\\" -> \\"merged4s\\" [label=left]
       \\"4prime\\" -> \\"merged4s\\" [label=right]
       }"
@@ -323,12 +323,12 @@ describe('GraphVisualizers', () => {
     expect(getTestDotGraph(commits, (commit) => commit.metadata).graph)
       .toMatchInlineSnapshot(`
       "digraph {
-      \\"1\\" [shape=ellipse, label=\\"1\\", color=black, fillcolor=azure, style=filled]
-      \\"2:4\\" [shape=ellipse, label=\\"2:4 (3 commits)\\", color=black, fillcolor=azure, style=filled]
+      \\"1\\" [shape=ellipse, label=\\"1\\", color=black, fillcolor=azure, style=filled, id=\\"1\\"];
+      \\"2:4\\" [shape=ellipse, label=\\"2:4 (3 commits)\\", color=black, fillcolor=azure, style=filled, id=\\"4\\"];
       \\"1\\" -> \\"2:4\\" [label=\\"\\"]
-      \\"2prime\\" [shape=ellipse, label=\\"2prime\\", color=black, fillcolor=azure, style=filled]
+      \\"2prime\\" [shape=ellipse, label=\\"2prime\\", color=black, fillcolor=azure, style=filled, id=\\"2prime\\"];
       \\"1\\" -> \\"2prime\\" [label=\\"third\\"]
-      \\"5\\" [shape=rectangle, label=\\"5\\", color=black, fillcolor=azure, style=filled]
+      \\"5\\" [shape=rectangle, label=\\"5\\", color=black, fillcolor=azure, style=filled, id=\\"5\\"];
       \\"2:4\\" -> \\"5\\" [label=left]
       \\"2prime\\" -> \\"5\\" [label=right]
       }"
@@ -435,20 +435,20 @@ describe('GraphVisualizers', () => {
       metadata: commit.edit.message,
     }));
 
-    expect(getTestDotGraph(commits, (commit) => commit.metadata))
+    expect(getTestDotGraph(commits, (commit) => commit.metadata).graph)
       .toMatchInlineSnapshot(`
       "digraph {
-      \\"middle-merge-base-ref\\" [shape=ellipse, label=\\"middle-merge-base-ref\\", color=black, fillcolor=azure, style=filled]
-      \\"middle-merge-merge-ref\\" [shape=ellipse, label=\\"middle-merge-merge-ref\\", color=black, fillcolor=azure, style=filled]
-      \\"middle-merge\\" [shape=rectangle, label=\\"middle-merge\\", color=black, fillcolor=azure, style=filled]
+      \\"middle-merge-base-ref\\" [shape=ellipse, label=\\"middle-merge-base-ref\\", color=black, fillcolor=azure, style=filled, id=\\"middle-merge-base-ref\\"];
+      \\"middle-merge-merge-ref\\" [shape=ellipse, label=\\"middle-merge-merge-ref\\", color=black, fillcolor=azure, style=filled, id=\\"middle-merge-merge-ref\\"];
+      \\"middle-merge\\" [shape=rectangle, label=\\"middle-merge\\", color=black, fillcolor=azure, style=filled, id=\\"middle-merge\\"];
       \\"middle-merge-base-ref\\" -> \\"middle-merge\\" [label=left]
       \\"middle-merge-merge-ref\\" -> \\"middle-merge\\" [label=right]
-      \\"last-merge-base-ref\\" [shape=ellipse, label=\\"last-merge-base-ref\\", color=black, fillcolor=azure, style=filled]
+      \\"last-merge-base-ref\\" [shape=ellipse, label=\\"last-merge-base-ref\\", color=black, fillcolor=azure, style=filled, id=\\"last-merge-base-ref\\"];
       \\"middle-merge-base-ref\\" -> \\"last-merge-base-ref\\" [label=\\"convert Overdub to audio\\"]
-      \\"last-merge-2\\" [shape=rectangle, label=\\"last-merge-2\\", color=black, fillcolor=azure, style=filled]
+      \\"last-merge-2\\" [shape=rectangle, label=\\"last-merge-2\\", color=black, fillcolor=azure, style=filled, id=\\"last-merge-2\\"];
       \\"last-merge-base-ref\\" -> \\"last-merge-2\\" [label=left]
       \\"middle-merge\\" -> \\"last-merge-2\\" [label=right]
-      \\"last-merge-1\\" [shape=rectangle, label=\\"last-merge-1\\", color=black, fillcolor=azure, style=filled]
+      \\"last-merge-1\\" [shape=rectangle, label=\\"last-merge-1\\", color=black, fillcolor=azure, style=filled, id=\\"last-merge-1\\"];
       \\"last-merge-base-ref\\" -> \\"last-merge-1\\" [label=left]
       \\"middle-merge\\" -> \\"last-merge-1\\" [label=right]
       }"
